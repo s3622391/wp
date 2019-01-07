3f678404a87a82da3cfeb1df45ac1169c66968de
s3622391 <s3622391@student.rmit.edu.au>
2019-01-07 17:33:55 +1100

    testing

--- a2/index.php
+++ a2/index.php
@@ -13,35 +13,35 @@
 </head>
 
 <body>
+    <background-color: rgb(300,200,000);>
+        <header>
+            <div>
 
-    <header>
-        <div>
+                <p><img src='../../media/cinema-logo.png' width='200' height='200' /> <strong>Numpty Cinemas</strong></p>
 
-            <p><img src='../../media/cinema-logo.png' width='200' height='200' /> <strong>Numpty Cinemas</strong></p>
+            </div>
+        </header>
 
-        </div>
-    </header>
+        <nav>
+            <div>Put website navigation links here</div>
+        </nav>
 
-    <nav>
-        <div>Put website navigation links here</div>
-    </nav>
+        <main>
+            <article id='Website Under Construction'>
+                <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
+                <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
+            </article>
+        </main>
 
-    <main>
-        <article id='Website Under Construction'>
-            <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-            <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
-        </article>
-    </main>
+        <footer>
+            <div>&copy;<script>
+                    document.write(new Date().getFullYear());
 
-    <footer>
-        <div>&copy;<script>
-                document.write(new Date().getFullYear());
-
-            </script> Put your name(s), student number(s) and group name here. Last modified
-            <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
-        <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
-        <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
-    </footer>
+                </script> Put your name(s), student number(s) and group name here. Last modified
+                <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+            <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+            <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+        </footer>
 
 </body>
 
