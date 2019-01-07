f62f3e835cc02c2f52bd00e7c6415c6d8964ed84
s3622391 <s3622391@student.rmit.edu.au>
2019-01-07 18:48:27 +1100

    Footer alterations

--- a2/index.php
+++ a2/index.php
@@ -54,7 +54,7 @@
         <div>&copy;<script>
                 document.write(new Date().getFullYear());
 
-            </script> Ian Dowdle, student number (s3622391). Last modified
+            </script> Ian Dowdle, student number (s3622391) <a href="https://github.com/s3622391/wp">GitHub Repository</a>. Last modified
             <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
         <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
         <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
