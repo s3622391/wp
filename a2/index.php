21cae5eb5da110a178cd822773a44717f445e113
s3622391 <s3622391@student.rmit.edu.au>
2019-01-10 08:27:18 +1100

    CSS work around7

--- a2/index.php
+++ a2/index.php
@@ -8,7 +8,7 @@
 
     <!-- Keep wireframe.css for debugging, add your css to style.css -->
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
-    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
+    <!--<link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css"> -->
     <script src='../wireframe.js'></script>
     <link href="https://fonts.googleapis.com/css?family=Bree+Serif|Coiny" rel="stylesheet">
 
