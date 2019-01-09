ecc339e2780e502ecb964b8e1f7f2693b88d79c3
s3622391 <s3622391@student.rmit.edu.au>
2019-01-10 10:16:13 +1100

    image float seats

--- a2/index.php
+++ a2/index.php
@@ -8,12 +8,12 @@
 
     <!-- Keep wireframe.css for debugging, add your css to style.css -->
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
-    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css"> 
+    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
     <script src='../wireframe.js'></script>
     <link href="https://fonts.googleapis.com/css?family=Bree+Serif|Coiny" rel="stylesheet">
 
-               
-     <!--   /* <style>
+
+    <!--   /* <style>
          body {
             height: auto;
             background-color: chocolate;
@@ -58,63 +58,78 @@
         
         } -->
 
-        </head>
-            <body>
-                <header>
-                        <div>
-                            <p id="top"><img src='../../media/cinema-logo.png'alt="Cinema Logo"width='200'height='200'class="align-centre"/></p>
-                            <p id="name">Numpty Cinemas</p>
-                        </div>
-                </header>
-            <nav id="navbar">
-                <div class="sticky">
-                    <ul>
-                        <li class="one"><a href="#About">About Us </a></li>
-                        <li class="one"><a href="#SeatsandPricing">Seats and Pricing</a></li>
-                        <li class="one"><a href="#NowShowing">Now Showing</a></li>
-                        <li class="one"><a href="#Booking">Booking</a><br /></li>
-                    </ul><
-                /div>
-                
-            </nav>
-            <main>
-                <div class="full-height">
-                    <h2 id="About">About Us</h2>
-                    <p></p>
-                </div>
-                <div class="full-height">
-                    <h2 id="SeatsandPricing">Seats and Pricing</h2>
-                        <img src='../../media/StandardSeat.png'alt='Standard Cinema Seat'height="300"/>
-                        <img src='../../media/FirstClassSeat.png'alt='First Class Cinema Seat'height="300"/>
-                        <br>
-                        <p id="textfont">New standard and First class seating available.</p>
-                </div>
-                <div class="full-height">
-                        <h2 id="NowShowing">Now Showing<h2><img src='../../media/avatar.png'alt='Avatar'class="align-centre"/>
-                        <p id="textfont">Now Showing Here.</p>
-                </div>
-                <div class="full-height">
-                        <h2 id="Booking">Booking<h2><img src='../../media/RMIT-Logo.png'alt='RMIT-Logo'/>
-                        <p id="textfont">Book Here.</p>
-                </div>
-        
-                <p><a href="#top">Top</a></p>
-            </main>
-        
-            <footer>
-                <div>
-                        <p><a href="mailto:NumptyCinema@example.com">Email US !</a>Phone: TBA, Address: TBA<br /></p></div><div>&copy;
-                        <script>document.write(new Date().getFullYear());
+</head>
 
-                        </script>Ian Dowdle, student number (s3622391) 
-                        <a href="https://github.com/s3622391/wp">GitHub Repository</a>. Last modified <?=date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME']));
-        ?>.
+<body>
+    <header>
+        <div>
+            <p id="top"><img src='../../media/cinema-logo.png' alt="Cinema Logo" width='200' height='200' class="align-centre" /></p>
+            <p id="name">Numpty Cinemas</p>
+        </div>
+    </header>
+
+    <nav id="navbar">
+        <div class="sticky">
+            <ul>
+                <li class="one"><a href="#About">About Us </a></li>
+                <li class="one"><a href="#SeatsandPricing">Seats and Pricing</a></li>
+                <li class="one"><a href="#NowShowing">Now Showing</a></li>
+                <li class="one"><a href="#Booking">Booking</a><br /></li>
+            </ul>
+        </div>
+    </nav>
+    <main>
+        <div>
+            <div class="full-height">
+                <h2 id="About">About Us</h2>
+                <p>I'm not sure what to put here</p>
+            </div>
+
+            <div class="full-height">
+                <h2 id="SeatsandPricing">Seats and Pricing</h2>
+                <div class="row">
+                    <div class="column">
+                        <img src='../../media/StandardSeat.png' alt='Standard Cinema Seat' height="300" />
+                    </div>
+                    <div class="column">
+                        <img src='../../media/FirstClassSeat.png' alt='First Class Cinema Seat' height="300" />
+                    </div>
                 </div>
-                <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.
-                </div>
-                <div>
-                        <button id='toggleWireframeCSS'onclick='toggleWireframe()'>Toggle Wireframe CSS</button>
-                </div>
-            </footer>
-          </body>
-    </html>
+                <p id="textfont">New standard and First class seating available.</p>
+            </div>
+
+            <div class="full-height">
+                <h2 id="NowShowing">Now Showing<h2>
+                        <img src='../../media/avatar.png' alt='Avatar' class="align-centre" />
+                        <p id="textfont">Now Showing Here.</p>
+            </div>
+
+            <div class="full-height">
+                <h2 id="Booking">Booking<h2><img src='../../media/RMIT-Logo.png' alt='RMIT-Logo' />
+                        <p id="textfont">Book Here.</p>
+            </div>
+        </div>
+        <p><a href="#top">Top</a></p>
+    </main>
+
+    <footer>
+        <div>
+            <p><a href="mailto:NumptyCinema@example.com">Email US !</a>Phone: TBA, Address: TBA<br /></p>
+        </div>
+        <div>&copy;
+            <script>
+                document.write(new Date().getFullYear());
+
+            </script>Ian Dowdle, student number (s3622391)
+            <a href="https://github.com/s3622391/wp">GitHub Repository</a>. Last modified
+            <?=date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME']));?>.
+        </div>
+        <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.
+        </div>
+        <div>
+            <button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button>
+        </div>
+    </footer>
+</body>
+
+</html>
