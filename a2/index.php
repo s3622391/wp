566fc5fcea1bcac996f52a979547ac305933ad7f
s3622391 <s3622391@student.rmit.edu.au>
2019-01-10 08:30:35 +1100

    CSS revert to original as it is not working correctly

--- a2/index.php
+++ a2/index.php
@@ -8,13 +8,11 @@
 
     <!-- Keep wireframe.css for debugging, add your css to style.css -->
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
-    <!--<link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css"> -->
+    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css"> 
     <script src='../wireframe.js'></script>
     <link href="https://fonts.googleapis.com/css?family=Bree+Serif|Coiny" rel="stylesheet">
 
-    <style>
-         <?php include("css/style.css"); // or wherever your css file is located ?>
-    <style>           
+               
         /* <style>
          body {
             height: auto;
