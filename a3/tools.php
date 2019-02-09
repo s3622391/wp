ec8eb99d319630b36ec9e74a58a8ce9b669accd7
s3622391 <s3622391@student.rmit.edu.au>
2019-02-09 12:02:24 +1100

    PHP tools - exess bracket

--- a3/tools.php
+++ a3/tools.php
@@ -22,16 +22,14 @@ printMyCode()
 */
 $name=''; 
     
-if(!empty ($_POST['submitted'])){
+if(!empty ($_POST['submitted']))
+{
     $name = trim($_POST['cust[name]']);
     if (empty($name)){
         $name_error='Name must be entered'
     }
-}  
-    
-    
-    
-    
+}     
+       
 /*
 function preShow( $arr, $returnAsString=false ) {
 $ret = '<pre>' . print_r($arr, true) . '</pre>';
